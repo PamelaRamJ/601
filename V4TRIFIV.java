1c25a3f2cc7ac2d0aabe76d1be007aa547f8366b
Pamela Ramírez Juarez <134025407+PamelaRamJ@users.noreply.github.com>
2023-07-05 10:43:38 -0600

    Se Agrega V4TRIFIV  y  FuncionMultiplo

---
+++ V4TRIFIV.java
@@ -0,0 +1,27 @@
+
+package pkg5.v4trifiv;
+
+
+import funciones.funcionMultiplo;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class V4TRIFIV {
+
+  
+    public static void main(String[] args) throws IOException {
+        //DECLARA DE VARIBLES
+        BufferedReader bufEntrada=new BufferedReader(new InputStreamReader(System.in));
+         int numero;
+         //OBJETO 
+         funcionMultiplo mult = new funcionMultiplo();
+         //ENTRADA DE DATOS 
+         System.out.println("Dame el numero");
+          numero = Integer.parseInt(bufEntrada.readLine());
+          //SALIDA DE DATOS
+           System.out.println(mult.retornaMultiplo(numero));                    
+    }
+    
+    
+}
---
+++ funcionMultiplo.java
@@ -0,0 +1,15 @@
+
+package funciones;
+
+public class funcionMultiplo {
+    public String retornaMultiplo(int num){
+        if( num % 3 == 0 && num % 5 == 0){
+             return "TRIFV"; 
+         }else if(num % 3 == 0){
+             return "TRI"; 
+         }else if(num % 5 == 0){
+             return"FIV";  }
+       
+        return "Ningun Valor";    
+    }
+}

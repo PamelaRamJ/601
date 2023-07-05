da3fb6442db96f2d914aa7c3e29fcff6d28c31de
Pamela Ramírez Juarez <134025407+PamelaRamJ@users.noreply.github.com>
2023-07-05 10:41:43 -0600

    Se Agrega V2TRIFIV

---
+++ V2TRIFIV.java
@@ -0,0 +1,36 @@
+package pkg3.v2trifiv;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class V2TRIFIV {
+
+   
+    public static void main(String[] args) throws IOException {
+        //DECLARACION DE VARIABLES
+         BufferedReader bufEntrada=new BufferedReader(new InputStreamReader(System.in));
+         int numero;
+         int mult3;
+         int mult5;
+         //ENTRA DE DATOS
+         System.out.println("Dame el numero");
+         numero = Integer.parseInt(bufEntrada.readLine());
+         
+         //PROCESAR DATOS
+         mult3 = numero % 3;
+         mult5 = numero % 5;
+         
+         //SALIDA DE DATOS
+         if( mult3 == 0 && mult5 == 0){
+             System.out.println("TRIFV"); 
+         }else if(mult3 == 0){
+             System.out.println("TRI"); 
+         }else if(mult5 == 0){
+             System.out.println("FIV");  
+         }
+          
+    }
+    
+}

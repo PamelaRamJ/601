dc0617446b8911cf9bbace2608707bd83c3240c2
Pamela Ramírez Juarez <134025407+PamelaRamJ@users.noreply.github.com>
2023-07-05 10:48:12 -0600

    Se Agrega Excepciones5

---
+++ Excepcion5.java
@@ -0,0 +1,22 @@
+package excepcion5;
+
+public class Excepcion5 {
+
+    public static void main(String[] args) {
+           try{
+       int divicion = miMetodo(10,0);    
+       System.out.println(divicion);
+       }catch(ArithmeticException e){
+       
+       System.out.println("Excepcion aritmetica arrojada");
+       e.printStackTrace();
+       }
+    }
+    public static int miMetodo(int a, int b) throws ArithmeticException{
+        if(b==0){
+            throw new ArithmeticException();
+        }
+        int c= a/b;
+       return c;
+    }
+}
